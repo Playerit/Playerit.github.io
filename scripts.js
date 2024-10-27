0658e0e94738e3fea101acaf5b1378bb5d73d0f8
skyandres123 <144279879+skyandres123@users.noreply.github.com>
2024-10-27 17:21:03 -0400

    Update scripts.js
    Added the event listener for the new button "Email us" and also added a function that opens an emailing that will automatically fill the "to" part of the email with "plinker@gmail.com"

--- scripts.js
+++ scripts.js
@@ -14,3 +14,13 @@ function sendMessage() {
     const messageText = messageCount === 1 ? 'message sent' : 'messages sent';
     document.getElementById('message').innerText = `(${messageCount}) ${messageText}`; // Display count
 }
+// function that opens your email app and enters automtically the playerink@gmail.com when you open the app
+function openEmailClient() {
+    window.location.href = 'mailto:playerink@gmail.com';
+}
+
+// Add event listener for email us button
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('emailButton').addEventListener('click', openEmailClient);
+});
+
